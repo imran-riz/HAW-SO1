0a8f8bdcd5c54950e6337e2c80448903aae5baa4
Imran Rizwan <imran.rizwan@scayle.com>
2025-01-12 10:50:59 +0100

    Mathematics bisection function

--- Exercises/AfterLec5/Bisection.c
+++ Exercises/AfterLec5/Bisection.c
@@ -1,6 +1,50 @@
+/**
+ *  A program with a bisection function to approximate zero-crossing of f(x) = x^2 - 2x - 1
+ */
+
 #include <stdio.h>
+
+double bisection(double xL, double xR, double intervalSize);
+double f(double x);
 
 int main(void)
 {
+    printf("%.9f", bisection(0, 4, 10e-6));
+    return 0;
+}
 
+double bisection(double xL, double xR, double intervalSize)
+{
+    double deltaX = xL - xR;
+    double fxL = 0.0, fxR = 0.0, xM, fxM;
+
+    while (deltaX < intervalSize)
+    {
+        xM = 0.5 * (xL + xR);
+
+        fxL = f(xL);
+        fxR = f(xR);
+        fxM = f(xM);
+
+        if (xL * xM > 0)
+        {
+            xL = xM;
+            fxL = fxM;
+        }
+        else
+        {
+            xR = xM;
+            fxR = fxM;
+        }
+
+        deltaX = xR - xL;
+    }
+
+    return 0.5 * (fxL + fxR);
+}
+
+// f(x) = x^2 - 2x - 1
+double f(double x)
+{
+    return (x*x) - 2.0*x - 1.0;
 }
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/imran.rizwan/Documents/Uni/SO1/UniWorkC
+# Install script for directory: /Users/imran.rizwan/Documents/Uni/Semester 1/SO1/UniWorkC
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "/Users/imran.rizwan/Documents/Uni/SO1/UniWorkC/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/Users/imran.rizwan/Documents/Uni/Semester 1/SO1/UniWorkC/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
